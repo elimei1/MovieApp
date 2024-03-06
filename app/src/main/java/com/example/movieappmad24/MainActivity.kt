5b6c5ef60c777aba41e6424e9b7eabe9d27fbdd6
elimei1 <112986140+elimei1@users.noreply.github.com>
2024-03-06 20:01:39 +0100

    class finished

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,15 +19,22 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -58,19 +66,16 @@ class MainActivity : ComponentActivity() {
     fun MovieRow(movie: Movie) {
         Card (
             modifier = Modifier
-            .fillMaxWidth()
-            .padding(5.dp),
-            shape = RoundedCornerShape(size = 20.dp)
+                .fillMaxWidth()
+                .padding(5.dp),
+            shape = RoundedCornerShape(size = 20.dp),
+
         ){
-
-
             Column {
                 Box {
-
-
                     Image(
                         painter = painterResource(id = R.drawable.movie_image),
-                        contentDescription = "placeholder_image",
+                        contentDescription = "placeholder",
                         contentScale = ContentScale.FillWidth,
                         modifier = Modifier
                             .aspectRatio(ratio= 18.5f/9f)
@@ -81,9 +86,12 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .align(alignment = Alignment.TopEnd)
                             .padding(5.dp),
-                        tint = Color.Red
+                        tint = MaterialTheme.colorScheme.secondary
                     )
 
+                }
+                var open by remember {
+                    mutableStateOf(false)
                 }
                 Row (
                     verticalAlignment = Alignment.CenterVertically
@@ -95,12 +103,18 @@ class MainActivity : ComponentActivity() {
                             .weight(weight = 7f),
                         fontSize = 18.sp
                     )
+
                     Icon(
-                        imageVector = Icons.Default.KeyboardArrowUp,
+                        imageVector =
+                            if (open) Icons.Default.KeyboardArrowDown
+                            else Icons.Default.KeyboardArrowUp,
                         contentDescription = "arrow",
                         modifier = Modifier
-                            .padding(end = 5.dp)
+                            .clickable {
+                                open = !open
+                            }
                     )
+
                 }
             }
         }
